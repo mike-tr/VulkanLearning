8e4502a328015a7e3dd1c4208be08f86218645f7
Mike <Miket9717@gmail.com>
2021-08-29 00:56:22 +0300

    seperated engine from main

---
+++ internal/vkEngine/src/vulkanWSI.cpp
@@ -0,0 +1,9 @@
+#include "headers/engine.hpp"
+#include <cstring>
+#include <headers/vulkanValidation.hpp>
+#include <optional>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
